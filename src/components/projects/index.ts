f5477e84cbf5e2647cd5f8fee9d207490c8bdb16
Alez05 <cercel.alexandru436@gmail.com>
2025-09-15 20:14:58 +0100

    feat: project section setup - featuring project card. .

--- src/components/projects/index.ts
+++ src/components/projects/index.ts
@@ -3,6 +3,7 @@ export { Projects } from './project'
 
 // components
 export { HeroPr } from "./hero-pr";
+export { ProjectSection } from './projects-section';
 
 // types
 export type { THeroPr } from "./hero-pr";
